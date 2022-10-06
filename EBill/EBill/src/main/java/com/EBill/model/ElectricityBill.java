7441d1752053568337e007db34ba55ffce40b5ab
Anubhav141 <115158782+Anubhav141@users.noreply.github.com>
2022-10-06 05:28:15 -0700

    Add files via upload

---
+++ EBill/EBill/src/main/java/com/EBill/model/ElectricityBill.java
@@ -0,0 +1,5 @@
+package com.EBill.model;
+
+public class ElectricityBill {
+
+}
---
+++ EBill/EBill/src/test/java/com/EBill/EBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EBill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
